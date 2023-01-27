07dd454981b68e679c7bf61990fe4e9ced4bcc78
yerdi <yerdi.isman@gmail.com>
2023-01-27 22:30:04 +0600

    first commit

---
+++ CatProject/CatProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatProjectApp.swift
+//  CatProject
+//
+//  Created by Yerdaulet Ismanaliyev on 27.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CatProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
